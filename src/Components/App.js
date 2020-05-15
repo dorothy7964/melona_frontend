11fcfb6716771e5ffb0132d9077fd59e0f664778
sample <dorothy_007@naver.com>
2020-05-15 20:29:08 +0900

    #1.05 Toastify onSubmit

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { HashRouter as Router } from "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from "../Styles/Theme";
@@ -9,7 +11,7 @@ import Routes from "./Routes";
 
 const QUERY = gql`
     {
-        isLoggedIn @client(always: false)
+        isLoggedIn @client(always: true)
     }
 `;
 
@@ -25,6 +27,7 @@ export default () => {
                 <Router>
                     <Routes isLoggedIn={isLoggedIn} />
                 </Router>
+                <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
             </React.Fragment>
         </ThemeProvider>
     );
