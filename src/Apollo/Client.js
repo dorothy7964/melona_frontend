3b8be0bff6185451f4553b2d1489119e06581506
sample <dorothy_007@naver.com>
2020-05-16 04:35:46 +0900

    #15.02 Apollo Client - Subscriptions

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,6 +1,9 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { split } from 'apollo-link';
 import { createHttpLink } from "apollo-link-http";
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import { setContext } from 'apollo-link-context';
 import { resolvers } from "./LocalState";
 
@@ -8,6 +11,16 @@ const httpLink = new createHttpLink({
     uri: process.env.NODE_ENV === "development"
         ? "http://localhost:4000"
         : "https://melona-backend.herokuapp.com/",
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+    uri: process.env.NODE_ENV === "development"
+				? "ws://localhost:4000"
+        : "wss://melona-backend.herokuapp.com/",
+    options: {
+        reconnect: true
+    }
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -22,6 +35,19 @@ const authLink = setContext((_, { headers }) => {
     }
 });
 
+const link = split(
+    // split based on operation type
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        );
+    },
+    wsLink,
+    httpLink
+);
+
 const cache = new InMemoryCache();
 cache.writeData({
     data: {
@@ -30,7 +56,7 @@ cache.writeData({
 });
 
 export default new ApolloClient({
-    link: authLink.concat(httpLink),
+    link: authLink.concat(link),
     cache,
     resolvers
 });
