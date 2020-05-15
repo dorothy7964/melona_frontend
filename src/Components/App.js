3953569302595961d69764415abb1a52f8502ce9
sample <dorothy_007@naver.com>
2020-05-15 20:31:37 +0900

    #2.01 Footer UI part

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,16 +4,24 @@ import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from "../Styles/Theme";
 import Routes from "./Routes";
+import Footer from "./Footer";
 
 const QUERY = gql`
     {
         isLoggedIn @client(always: true)
     }
 `;
+
+const Wrapper = styled.div`
+    max-width: ${props => props.theme.maxWidthLarge};
+    margin: 0 auto;
+    width: 100%;
+`;
+
 
 export default () => {
     const {
@@ -24,9 +32,12 @@ export default () => {
         <ThemeProvider theme={Theme}>
             <React.Fragment>
                 <GlobalStyles />
-                <Router>
-                    <Routes isLoggedIn={isLoggedIn} />
-                </Router>
+                <Wrapper>
+                    <Router>
+                        <Routes isLoggedIn={isLoggedIn} />
+                        <Footer />
+                    </Router>
+                </Wrapper>
                 <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
             </React.Fragment>
         </ThemeProvider>
