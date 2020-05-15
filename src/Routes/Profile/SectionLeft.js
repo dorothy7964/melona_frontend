32d90083c2da9d8a8422b769233dc02f31d1078d
sample <dorothy_007@naver.com>
2020-05-16 07:07:56 +0900

    #16.01 add allGroupRoom Alert

--- src/Routes/Profile/SectionLeft.js
+++ src/Routes/Profile/SectionLeft.js
@@ -4,7 +4,24 @@ import { withRouter } from 'react-router-dom';
 import { useQuery } from "react-apollo-hooks";
 import { ALL_GROUPROOM } from "./ProfileQueries";
 import Loader from "../../Components/Loader";
+import FatText from "../../Components/FatText";
 import RoomCard from "../../Components/RoomCard";
+import togeter_grey from "../../Icons/togeter_grey.png";
+
+const IconBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+        width: 40%;
+    }
+    span {
+        margin-top: 20px;
+        color: ${props => props.theme.darkGreyColor}
+        font-size: 20pt;
+        font-weight: 100;
+    }
+`;
 
 const Section = styled.div`
     @media (max-width: ${props => props.theme.maxWidthLarge}){
@@ -44,23 +61,32 @@ export default withRouter(({ history }) =>  {
         );
     } else if (!loading && data && data.allGroupRoom) {
         const { allGroupRoom } = data;
-        
-        return (
-            <Section>
-                {allGroupRoom.map(groupRoom => (
-                    <RoomCard  
-                        key={groupRoom.id} 
-                        groupRoomId={groupRoom.id}
-                        roomName={groupRoom.roomName}
-                        coverPhoto={groupRoom.coverPhoto}
-                        founderUser={groupRoom.founderUser}
-                        groupRoomMember={groupRoom.groupRoomMember[0]}
-                        createdAt={groupRoom.createdAt}
-                        handleEnter={handleEnter}
 
-                    />
-                ))}
-            </Section>
-        );
+        if (allGroupRoom.length === 0) {
+            return (
+                <IconBox>
+                    <img alt="togeter_grey" src={togeter_grey} />
+                    <FatText text="그룹을 추가 해주세요." />
+                </IconBox>
+            );
+        } else {
+            return (
+                <Section>
+                    {allGroupRoom.map(groupRoom => (
+                        <RoomCard  
+                            key={groupRoom.id} 
+                            groupRoomId={groupRoom.id}
+                            roomName={groupRoom.roomName}
+                            coverPhoto={groupRoom.coverPhoto}
+                            founderUser={groupRoom.founderUser}
+                            groupRoomMember={groupRoom.groupRoomMember[0]}
+                            createdAt={groupRoom.createdAt}
+                            handleEnter={handleEnter}
+    
+                        />
+                    ))}
+                </Section>
+            );
+        }
     }
 });
