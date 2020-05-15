3a8bf9d48d9ca597f2659c63909811d025bd691e
sample <dorothy_007@naver.com>
2020-05-15 20:27:43 +0900

    #1.04 First Hooks Query

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { HashRouter as Router } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from "../Styles/Theme";
 import Routes from "./Routes";
 
+const QUERY = gql`
+    {
+        isLoggedIn @client(always: false)
+    }
+`;
+
 export default () => {
-  return (
-    <ThemeProvider theme={Theme}>
-        <React.Fragment>
-            <GlobalStyles />
-            <Router>
-                <Routes isLoggedIn={false} />
-            </Router>
-        </React.Fragment>
-    </ThemeProvider>
-  );
+    const {
+        data: { isLoggedIn }
+    } = useQuery(QUERY);
+
+	return (
+        <ThemeProvider theme={Theme}>
+            <React.Fragment>
+                <GlobalStyles />
+                <Router>
+                    <Routes isLoggedIn={isLoggedIn} />
+                </Router>
+            </React.Fragment>
+        </ThemeProvider>
+    );
 };
